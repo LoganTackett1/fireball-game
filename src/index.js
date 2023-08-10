d2b4ce823fa1ecf8002359cde7018c51b23e3de5
LoganTackett1 <logantackettwork@gmail.com>
2023-08-10 14:29:05 -0700

    render.js logic. setInterval loop for gameState and render. Fixed issue with decimal values in render functions.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import gameInit from "./game";
+import render from "./render";
 // eslint-disable-next-line no-unused-vars, import/no-unresolved
 import css from "./index.css";
 
@@ -5,3 +7,10 @@ const display = document.createElement("canvas");
 document.body.appendChild(display);
 display.width = 800;
 display.height = 400;
+
+const client = gameInit();
+
+setInterval(() => {
+  client.next();
+  render(display, client.gameState);
+}, 33);
---
+++ src/render.js
@@ -0,0 +1,25 @@
+function inputMap(canvas, x, y, width, height) {
+  return [x, canvas.height - y, width, -height];
+}
+
+function render(canvas, state) {
+  const ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.beginPath();
+  ctx.rect(...inputMap(0, 0, canvas.width, 100));
+  ctx.fillStyle = "Green";
+  ctx.fillRect();
+  ctx.closePath();
+
+  Object.keys(state).forEach((key) => {
+    const phys = state[key].physics;
+    ctx.beginPath();
+    ctx.rect(...inputMap(Math.round(phys.x - 15), Math.round(phys.y), 30, 30));
+    ctx.fillStyle = "Red";
+    ctx.fillRect();
+    ctx.closePath();
+  });
+}
+
+export default render;
